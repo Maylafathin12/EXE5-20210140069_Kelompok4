e600e10075d2775b1eebefa352fb18cdf9ca82b2
TA1069 <maylafaat@gmail.com>
2022-12-21 17:01:48 +0700

    add case 1,2,3,4

--- EXE5-20210140069_Kelompok4/Program.cs
+++ EXE5-20210140069_Kelompok4/Program.cs
@@ -54,5 +54,57 @@ namespace EXE5_20210140069_Kelompok4
             }
             Node display;
         }
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu ");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("1. Implement delete operation");
+                    Console.WriteLine("3. Display values ");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("\n Enter your choice (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.WriteLine("\nEnter a number: ");
+                                string nm = Console.ReadLine();
+                                Console.WriteLine();
+                                q.insert(nm);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.delete();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+
+                }
+            }
+        }
     }
-}
