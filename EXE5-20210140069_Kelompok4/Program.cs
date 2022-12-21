e4eca7e4bec60f7f8117f32ceba4a59bd4d42c11
TA1069 <maylafaat@gmail.com>
2022-12-21 15:36:36 +0700

    Add project files.

---
+++ EXE5-20210140069_Kelompok4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EXE5_20210140069_Kelompok4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
